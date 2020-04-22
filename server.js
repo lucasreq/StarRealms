e1016d36b308bba40bc7c775ee411cade0d7e670
lucasreq <lucas.requena@ynov.com>
2020-04-22 14:43:15 +0200

    init

---
+++ server.js
@@ -0,0 +1,32 @@
+const server = require('express')();
+const http = require('http').createServer(server);
+const io = require('socket.io')(http);
+let players = [];
+
+io.on('connection', function (socket) {
+    console.log('A user connected: ' + socket.id);
+
+    players.push(socket.id);
+
+    if (players.length === 1) {
+        io.emit('isPlayerA');
+        console.log(socket.id + " is player A")
+    };
+
+    socket.on('dealCards', function () {
+        io.emit('dealCards');
+    });
+
+    socket.on('cardPlayed', function (gameObject, isPlayerA) {
+        io.emit('cardPlayed', gameObject, isPlayerA);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('A user disconnected: ' + socket.id);
+        players = players.filter(player => player !== socket.id);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('Server started!');
+});
